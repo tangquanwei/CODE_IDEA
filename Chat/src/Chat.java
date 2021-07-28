bb456f7cd6164e654fc97f84f92523a1814e398d
Quanw <36056957+tangquanwei@users.noreply.github.com>
2021-07-28 21:59:51 +0800

    add a lot

--- Chat/src/Chat.java
+++
@@ -1,9 +0,0 @@
-public class Chat {
-    public static void main(String[] args) {
-        var server = new DayAdviceServer();
-        server.go();
-        var client = new DayAdviceClient();
-        client.go();
-
-    }
-}
